faf5b75db162e11b12a251b7d69448d08eadd14b
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-05 17:49:28 +0300

    update script

--- scripts/getDateDiff.sql
+++ scripts/getDateDiff.sql
@@ -1,8 +1,8 @@
 SELECT 
 	MAX(session_time) as 'session_time',
-	DATEDIFF(hh, GETDATE(), session_date) as 'time_hour_difference',
-	DATEDIFF(dd, GETDATE(), session_date) as 'time_day_difference',
-	DATEDIFF(mm, GETDATE(), session_date) as 'time_month_difference'
+	DATEDIFF(hour, GETDATE(), session_date) as 'time_hour_difference',
+	DATEDIFF(day, GETDATE(), session_date) as 'time_day_difference',
+	DATEDIFF(month, GETDATE(), session_date) as 'time_month_difference'
 FROM [smartace.analytics.sessionTime]
 WHERE 
 	session_time != 0
