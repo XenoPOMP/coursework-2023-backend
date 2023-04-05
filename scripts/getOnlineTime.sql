03fbd9a56d6fa7c389baa4f0f8e4e95d909594d1
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-05 04:10:32 +0300

    add sql script for loading online time

---
+++ scripts/getOnlineTime.sql
@@ -0,0 +1,12 @@
+
+SELECT MAX(session_time) as 'session_time', session_date
+FROM [smartace.analytics.sessionTime]
+WHERE session_time != 0
+GROUP BY session_token, session_date
+ORDER BY session_date ASC
+
+
+/* 
+DELETE FROM
+[smartace.analytics.sessionTime]
+*/
