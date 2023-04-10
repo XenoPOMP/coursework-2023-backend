e9c7601acba40a6f7756449ef2973deab883f80c
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-10 15:58:54 +0300

    add TODO

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import Env from '@type/Env';
 
+// TODO add tsconfig paths
+
 async function bootstrap() {
   require('dotenv').config();
   const env: Env = process.env;
