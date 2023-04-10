afb482f4afe13e86ef06269d255704489693e265
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-10 16:54:27 +0300

    delete datediff endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { OnlineModule } from '@/online/online.module';
-import { DatediffModule } from '@/datediff/datediff.module';
 import { DevicesModule } from '@/devices/devices.module';
 import { LoginModule } from '@/login/login.module';
 
 @Module({
-  imports: [OnlineModule, DatediffModule, DevicesModule, LoginModule],
+  imports: [OnlineModule, DevicesModule, LoginModule],
   controllers: [AppController],
   providers: [AppService],
 })
